865ce74994497c4333323621bbc1ca2c3a137bb7
Start <vl.zorov@gmail.com>
2022-08-17 17:30:47 +0300

    Add changes

---
+++ src/hotel/Person.java
@@ -0,0 +1,54 @@
+package hotel;
+
+public class Person {
+    private String name;
+    private int id;
+    private int age;
+    private String hometown;
+
+    public Person(String name, int id, int age, String hometown) {
+        setName(name);
+        setId(id);
+        setAge(age);
+        this.hometown = "n/a";
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+
+    @Override
+    public String toString() {
+        return String
+                .format("Person %s: %d, Age: %d, Hometown: %s"
+                        ,this.name, this.age, this.hometown);
+    }
+}
