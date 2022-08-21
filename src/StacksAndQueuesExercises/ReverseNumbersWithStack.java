23e8d5b448f5b6c297d032427f04dac4f613dc45
Start <vl.zorov@gmail.com>
2022-08-21 17:54:09 +0300

    Add changes

---
+++ src/StacksAndQueuesExercises/ReverseNumbersWithStack.java
@@ -0,0 +1,11 @@
+package StacksAndQueuesExercises;
+
+import java.util.Scanner;
+
+public class ReverseNumbersWithStack {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
